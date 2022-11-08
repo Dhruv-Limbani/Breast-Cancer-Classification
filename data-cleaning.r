daa78cd993ec68ef68eb040da961177df24ae132
Ahan <64554828+AhanR@users.noreply.github.com>
2022-11-09 00:53:59 +0530

    plot pairwise and analyse and keep data

--- data-cleaning.r
+++ data-cleaning.r
@@ -1,18 +1,34 @@
+#install.packages("ggplot2")
+#install.packages("GGally")
+
+
+library("magrittr")
+library("ggcorrplot")
+library("ggplot2")
+library("GGally")
+
 data <- read.csv(file.choose(), header = TRUE)
+x <- data$X
+data <- subset (data, select = -c(X, id))
 dim(data)
 
-numeric.data <- data.frame(
-  data$Age,
-  data$Tumor.Size,
-  data$Regional.Node.Examined,
-  data$Reginol.Node.Positive,
-  data$Survival.Months,
-  as.numeric(data$Grade)
-)
+data$diagnosis = factor(data$diagnosis)
 
-install.packages("corrplot")
-library("corrplot")
-corrplot(cor(numeric.data))
+head(data)
 
+pairPlot <- ggpairs(data)
+pairPlot
 
-#convert other data into classes
+# cor(data) %>% ggcorrplot(show.diag = FALSE, lab = TRUE)
+
+useless.features <- c("compactness_se", "texture_mean,radius_se", "texture_se", "perimeter_se", "area_se,smoothness_se","fractal_dimension_mean", "concavity_se", "fractal_dimension_worst", "symmetry_worst","texture_worst")
+lin.data = subset(data, select = -c(compactness_se, texture_mean,radius_se, texture_se, perimeter_se, area_se,smoothness_se,fractal_dimension_mean, concavity_se, fractal_dimension_worst, symmetry_worst,texture_worst))
+
+pairPlot <- ggpairs(lin.data)
+pairPlot
+
+boxplot(lin.data$radius_mean)
+boxplot(lin.data$perimeter_mean)
+boxplot(lin.data$area_mean)
+boxplot(lin.data$smoothness_mean)
+boxplot(lin.data$compactness_mean)
