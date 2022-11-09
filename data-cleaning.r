849c3bbc60d7eccf7ca15de8c000d163a4564659
Ahan <64554828+AhanR@users.noreply.github.com>
2022-11-09 22:37:27 +0530

    Cleaned data and selected linear variables

--- data-cleaning.r
+++ data-cleaning.r
@@ -1,7 +1,8 @@
 #install.packages("ggplot2")
 #install.packages("GGally")
+#install.packages("reshape")
 
-
+library("reshape")
 library("magrittr")
 library("ggcorrplot")
 library("ggplot2")
@@ -19,16 +20,15 @@ head(data)
 pairPlot <- ggpairs(data)
 pairPlot
 
-# cor(data) %>% ggcorrplot(show.diag = FALSE, lab = TRUE)
-
-useless.features <- c("compactness_se", "texture_mean,radius_se", "texture_se", "perimeter_se", "area_se,smoothness_se","fractal_dimension_mean", "concavity_se", "fractal_dimension_worst", "symmetry_worst","texture_worst")
 lin.data = subset(data, select = -c(compactness_se, texture_mean,radius_se, texture_se, perimeter_se, area_se,smoothness_se,fractal_dimension_mean, concavity_se, fractal_dimension_worst, symmetry_worst,texture_worst))
 
 pairPlot <- ggpairs(lin.data)
 pairPlot
 
-boxplot(lin.data$radius_mean)
-boxplot(lin.data$perimeter_mean)
-boxplot(lin.data$area_mean)
-boxplot(lin.data$smoothness_mean)
-boxplot(lin.data$compactness_mean)
+
+meltData <- melt(data)
+boxplot(data = meltData, value~variable )
+
+write.csv(lin.data, file.choose())
+
+# The variables with the most outliers are area_mean and area_worst
